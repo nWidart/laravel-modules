6ed0688f97b67af1c82cc12f62fefacffce7dc5d
Nicolas Widart <n.widart@gmail.com>
2016-06-29 15:23:51 +0200

    Test laravel module facade

---
+++ tests/ModuleFacadeTest.php
@@ -0,0 +1,14 @@
+<?php namespace Nwidart\Modules\Tests;
+
+use Nwidart\Modules\Facades\Module;
+
+class ModuleFacadeTest extends BaseTestCase
+{
+    /** @test */
+    public function it_resolves_the_module_facade()
+    {
+        $modules = Module::all();
+
+        $this->assertTrue(is_array($modules));
+    }
+}
