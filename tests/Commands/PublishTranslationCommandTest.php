85475e7dbf225a829bd01ef0306740fff6ef2d52
Nicolas Widart <n.widart@gmail.com>
2016-06-29 15:33:19 +0200

    Test module translations are getting published

---
+++ tests/Commands/PublishTranslationCommandTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Nwidart\Modules\Tests\Commands;
+
+use Nwidart\Modules\Tests\BaseTestCase;
+
+class PublishTranslationCommandTest extends BaseTestCase
+{
+    /**
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    private $finder;
+    /**
+     * @var string
+     */
+    private $modulePath;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->modulePath = base_path('modules/Blog');
+        $this->finder = $this->app['files'];
+        $this->artisan('module:make', ['name' => ['Blog']]);
+    }
+
+    public function tearDown()
+    {
+        $this->finder->deleteDirectory($this->modulePath);
+        parent::tearDown();
+    }
+
+    /** @test */
+    public function it_published_module_translations()
+    {
+        $this->artisan('module:publish-translation', ['module' => 'Blog']);
+
+        $this->assertTrue(is_dir(base_path('resources/lang/blog')));
+    }
+}
