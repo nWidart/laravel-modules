977dba0bec57cd02c8f37c39798da82038ca6944
Nicolas Widart <n.widart@gmail.com>
2016-06-29 14:51:01 +0200

    Test a migration file was generated

---
+++ tests/Commands/MigrationCommandTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Nwidart\Modules\Tests\Commands;
+
+use Nwidart\Modules\Tests\BaseTestCase;
+
+class MigrationCommandTest extends BaseTestCase
+{
+    /**
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    private $finder;
+    /**
+     * @var string
+     */
+    private $modulePath;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->modulePath = base_path('modules/Blog');
+        $this->finder = $this->app['files'];
+        $this->artisan('module:make', ['name' => ['Blog']]);
+    }
+
+    public function tearDown()
+    {
+        $this->finder->deleteDirectory($this->modulePath);
+        parent::tearDown();
+    }
+
+    /** @test */
+    public function it_generates_a_new_migration_class()
+    {
+        $this->artisan('module:make-migration', ['name' => 'create_posts_table', 'module' => 'Blog']);
+
+        $files = $this->finder->allFiles($this->modulePath . '/Database/Migrations');
+
+        $this->assertCount(1, $files);
+    }
+}
