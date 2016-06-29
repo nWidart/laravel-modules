560cfb526b27dab46e0401c0dd190308a4d97aa6
Nicolas Widart <n.widart@gmail.com>
2016-06-29 16:35:04 +0200

    Prepare a test for migration testing

---
+++ tests/Commands/MigrateCommandTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Nwidart\Modules\Tests\Commands;
+
+use Illuminate\Support\Facades\Schema;
+use Nwidart\Modules\Repository;
+use Nwidart\Modules\Tests\BaseTestCase;
+
+abstract class MigrateCommandTest extends BaseTestCase
+{
+    /**
+     * @var Repository
+     */
+    private $repository;
+    /**
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    private $finder;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->repository = new Repository($this->app);
+        $this->finder = $this->app['files'];
+    }
+
+    /** @test */
+    public function it_migrates_a_module()
+    {
+        $this->repository->addLocation(__DIR__ . '/../stubs/Recipe');
+
+        $this->artisan('module:migrate', ['module' => 'Recipe']);
+
+        dd(Schema::hasTable('recipe__recipes') , $this->app['db']->table('recipe__recipes')->get());
+    }
+}
