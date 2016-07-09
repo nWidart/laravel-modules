f143251a830255713f82081cb4940538cbda09b6
Nicolas Widart <n.widart@gmail.com>
2016-07-09 21:44:39 +0200

    Adding trait to clean the modules cache

---
+++ src/Traits/CanClearModulesCache.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Nwidart\Modules\Traits;
+
+trait CanClearModulesCache
+{
+    /**
+     * Clear the modules cache if it is enabled
+     */
+    public function clearCache()
+    {
+        if (config('modules.cache.enabled') === true) {
+            app('cache')->forget('laravel-modules');
+        }
+    }
+}
