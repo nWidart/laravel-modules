2ed02d791bd9064ac34f6fc9bc3adb2a99f53400
Nicolas Widart <n.widart@gmail.com>
2016-08-24 10:31:03 +0200

    Simplify the array first usage

--- src/Support/Migrations/SchemaParser.php
+++ src/Support/Migrations/SchemaParser.php
@@ -227,9 +227,7 @@ class SchemaParser implements Arrayable
      */
     public function getColumn($schema)
     {
-        return array_first(explode(':', $schema), function ($key, $value) {
-            return $value;
-        });
+        return array_first(explode(':', $schema));
     }
 
     /**
