e3a967fb442b88dedb47eb1f303d3f3bbc1e19d4
John Tripi <tripij1@gmail.com>
2016-09-12 19:28:47 +0000

    fixes a bug which would occur if the migrate key exists but not a path to the migration scripts

--- src/Commands/MigrateCommand.php
+++ src/Commands/MigrateCommand.php
@@ -80,9 +80,11 @@ class MigrateCommand extends Command
      */
     protected function getPath($module)
     {
-        $config = $module->get('migrate', ['path' => config('modules.paths.generator.migration')]);
+        $config = $module->get('migrate');
 
-        $path = $module->getExtraPath($config['path']);
+        $path = (is_array($config) && array_key_exists('path', $config)) ? $config['path'] : config('modules.paths.generator.migration');
+
+        $path = $module->getExtraPath($path);
 
         return str_replace(base_path(), '', $path);
     }
