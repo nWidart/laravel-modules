2a2c3af2a347d2c8796d454d3187665f7ef6c3b3
Nicolas Widart <n.widart@gmail.com>
2017-05-21 18:33:00 +0200

    Remove the module.json from config file, it should always be generated.

--- config/config.php
+++ config/config.php
@@ -28,7 +28,6 @@ return [
         'files' => [
             'start' => 'start.php',
             'routes' => 'Http/routes.php',
-            'json' => 'module.json',
             'views/index' => 'Resources/views/index.blade.php',
             'views/master' => 'Resources/views/layouts/master.blade.php',
             'scaffold/config' => 'Config/config.php',
