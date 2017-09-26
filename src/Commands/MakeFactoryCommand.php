8f73cff850b046009dcc4bf4aee42fb38f1e3ccf
Martin Kiesel <martin.kiesel@gmail.com>
2017-09-26 10:27:23 +0200

    Description wording, styling
     - removing commented out code
     - removing unused lines

--- src/Commands/MakeFactoryCommand.php
+++ src/Commands/MakeFactoryCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputArgument;
 
 class MakeFactoryCommand extends GeneratorCommand
 {
+
     use ModuleCommandTrait;
 
     /**
@@ -32,6 +33,7 @@ class MakeFactoryCommand extends GeneratorCommand
      */
     protected $description = 'Create a new model factory for the specified module.';
 
+
     /**
      * Get the console command arguments.
      *
@@ -39,21 +41,21 @@ class MakeFactoryCommand extends GeneratorCommand
      */
     protected function getArguments()
     {
-        return array(
-            array('name', InputArgument::REQUIRED, 'The name of the factory.'),
-            array('module', InputArgument::OPTIONAL, 'The name of module will be used.'),
-        );
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the factory.'],
+            ['module', InputArgument::OPTIONAL, 'The name of module will be used.'],
+        ];
     }
+
 
     /**
      * @return mixed
      */
     protected function getTemplateContents()
     {
-        $module = $this->laravel['modules']->findOrFail($this->getModuleName());
-
         return (new Stub('/factory.stub'))->render();
     }
+
 
     /**
      * @return mixed
@@ -61,17 +63,16 @@ class MakeFactoryCommand extends GeneratorCommand
     protected function getDestinationFilePath()
     {
         $path = $this->laravel['modules']->getModulePath($this->getModuleName());
-
-        // $factoryPath = $this->laravel['modules']->config('paths.generator.factories');
-
-        return $path . 'Database/factories/' . $this->getFileName();
+        
+        return $path.'Database/factories/'.$this->getFileName();
     }
+
 
     /**
      * @return string
      */
     private function getFileName()
     {
-        return Str::studly($this->argument('name')) . '.php';
+        return Str::studly($this->argument('name')).'.php';
     }
 }
