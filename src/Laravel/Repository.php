758ad6a2810db06c804d7cdbd0ccbe39ef2eb2c1
balping <balping314@gmail.com>
2017-11-25 13:46:28 +0000

    remove code duplication

--- src/Laravel/Repository.php
+++ src/Laravel/Repository.php
@@ -10,40 +10,8 @@ class Repository extends BaseRepository
     /**
      * {@inheritdoc}
      */
-    public function scan()
+    protected function createModule(...$args)
     {
-        $paths = $this->getScanPaths();
-
-        $modules = [];
-
-        foreach ($paths as $key => $path) {
-            $manifests = $this->app['files']->glob("{$path}/module.json");
-
-            is_array($manifests) || $manifests = [];
-
-            foreach ($manifests as $manifest) {
-                $name = Json::make($manifest)->get('name');
-
-                $modules[$name] = new Module($this->app, $name, dirname($manifest));
-            }
-        }
-
-        return $modules;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function formatCached($cached)
-    {
-        $modules = [];
-
-        foreach ($cached as $name => $module) {
-            $path = $module["path"];
-
-            $modules[$name] = new Module($this->app, $name, $path);
-        }
-
-        return $modules;
+        return new Module(...$args);
     }
 }
