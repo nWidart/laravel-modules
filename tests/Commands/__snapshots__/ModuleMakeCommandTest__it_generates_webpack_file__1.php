69ef935f116956635f160b0182a9c6361ed15329
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2018-03-27 23:23:51 +0300

    Enable versioning in production

--- tests/Commands/__snapshots__/ModuleMakeCommandTest__it_generates_webpack_file__1.php
+++ tests/Commands/__snapshots__/ModuleMakeCommandTest__it_generates_webpack_file__1.php
@@ -4,4 +4,8 @@ mix.setPublicPath(\'public\')
     .copy(\'public\', __dirname + \'/../../public/modules/blog\')
 
 mix.js(__dirname + \'/Resources/assets/js/app.js\', \'public/js\')
-    .sass( __dirname + \'/Resources/assets/sass/app.scss\', \'public/css\');';
+    .sass( __dirname + \'/Resources/assets/sass/app.scss\', \'public/css\');
+
+if (mix.inProduction()) {
+    mix.version();
+}';
