1bf97c184ad80d70a623cf36cdf5c75a3db153ae
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2018-04-05 21:15:18 +0300

    Fix Laravel Mix

--- tests/Commands/__snapshots__/ModuleMakeCommandTest__it_generates_webpack_file__1.php
+++ tests/Commands/__snapshots__/ModuleMakeCommandTest__it_generates_webpack_file__1.php
@@ -1,10 +1,10 @@
 <?php return 'const { mix } = require(\'laravel-mix\');
+require(\'laravel-mix-merge-manifest\');
 
-mix.setPublicPath(\'public\')
-    .copy(\'public\', __dirname + \'/../../public/modules/blog\')
+mix.setPublicPath(\'../../public\').mergeManifest();
 
-mix.js(__dirname + \'/Resources/assets/js/app.js\', \'public/js\')
-    .sass( __dirname + \'/Resources/assets/sass/app.scss\', \'public/css\');
+mix.js(__dirname + \'/Resources/assets/js/app.js\', \'js/blog.js\')
+    .sass( __dirname + \'/Resources/assets/sass/app.scss\', \'css/blog.css\');
 
 if (mix.inProduction()) {
     mix.version();
