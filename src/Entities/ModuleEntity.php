68be941468e6c0877e5b6c0e9a952c17a37be220
Nicolas Widart <n.widart@gmail.com>
2018-07-01 21:54:25 +0200

    Adding simple @properties

--- src/Entities/ModuleEntity.php
+++ src/Entities/ModuleEntity.php
@@ -4,6 +4,19 @@ namespace Nwidart\Modules\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string name
+ * @property string path
+ * @property string alias
+ * @property string description
+ * @property string keywords
+ * @property boolean is_active
+ * @property int order
+ * @property string providers
+ * @property string aliases
+ * @property string files
+ * @property string requires
+ */
 class ModuleEntity extends Model
 {
     public $table = 'modules';
