91d3697b216370b5bc1de2731911043ad60f0c6e
Nicolas Widart <n.widart@gmail.com>
2019-08-19 16:39:31 +0200

    Moving private methods down

--- src/Activators/FileActivator.php
+++ src/Activators/FileActivator.php
@@ -5,6 +5,7 @@ namespace Nwidart\Modules\Activators;
 use Illuminate\Cache\CacheManager;
 use Illuminate\Config\Repository as Config;
 use Illuminate\Container\Container;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Filesystem\Filesystem;
 use Nwidart\Modules\Contracts\ActivatorInterface;
 use Nwidart\Modules\Module;
@@ -65,65 +66,6 @@ class FileActivator implements ActivatorInterface
         $this->cacheKey = $this->config('cache-key');
         $this->cacheLifetime = $this->config('cache-lifetime');
         $this->modulesStatuses = $this->getModulesStatuses();
-    }
-
-    /**
-     * Get modules statuses, either from the cache or
-     * from the json statuses file if the cache is disabled.
-     * @return array
-     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
-     */
-    private function getModulesStatuses(): array
-    {
-        if (!$this->config->get('modules.cache.enabled')) {
-            return $this->readJson();
-        }
-
-        return $this->cache->remember($this->cacheKey, $this->cacheLifetime, function () {
-            return $this->readJson();
-        });
-    }
-
-    /**
-     * Flushes the modules activation statuses cache
-     */
-    private function flushCache()
-    {
-        $this->cache->forget($this->cacheKey);
-    }
-
-    /**
-     * Reads a config parameter
-     *
-     * @param  string $key     [description]
-     * @param  $default
-     * @return mixed
-     */
-    private function config(string $key, $default = null)
-    {
-        return $this->config->get('modules.activators.file.' . $key, $default);
-    }
-
-    /**
-     * Reads the json file that contains the activation statuses.
-     * @return array
-     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
-     */
-    private function readJson(): array
-    {
-        if (!$this->files->exists($this->statusesFile)) {
-            return [];
-        }
-
-        return json_decode($this->files->get($this->statusesFile), true);
-    }
-
-    /**
-     * Writes the activation statuses in a file, as json
-     */
-    private function writeJson(): void
-    {
-        $this->files->put($this->statusesFile, json_encode($this->modulesStatuses, JSON_PRETTY_PRINT));
     }
 
     /**
@@ -205,5 +147,64 @@ class FileActivator implements ActivatorInterface
         unset($this->modulesStatuses[$module->getName()]);
         $this->writeJson();
         $this->flushCache();
+    }
+
+    /**
+     * Writes the activation statuses in a file, as json
+     */
+    private function writeJson(): void
+    {
+        $this->files->put($this->statusesFile, json_encode($this->modulesStatuses, JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * Reads the json file that contains the activation statuses.
+     * @return array
+     * @throws FileNotFoundException
+     */
+    private function readJson(): array
+    {
+        if (!$this->files->exists($this->statusesFile)) {
+            return [];
+        }
+
+        return json_decode($this->files->get($this->statusesFile), true);
+    }
+
+    /**
+     * Get modules statuses, either from the cache or from
+     * the json statuses file if the cache is disabled.
+     * @return array
+     * @throws FileNotFoundException
+     */
+    private function getModulesStatuses(): array
+    {
+        if (!$this->config->get('modules.cache.enabled')) {
+            return $this->readJson();
+        }
+
+        return $this->cache->remember($this->cacheKey, $this->cacheLifetime, function () {
+            return $this->readJson();
+        });
+    }
+
+    /**
+     * Reads a config parameter under the 'activators.file' key
+     *
+     * @param  string $key
+     * @param  $default
+     * @return mixed
+     */
+    private function config(string $key, $default = null)
+    {
+        return $this->config->get('modules.activators.file.' . $key, $default);
+    }
+
+    /**
+     * Flushes the modules activation statuses cache
+     */
+    private function flushCache()
+    {
+        $this->cache->forget($this->cacheKey);
     }
 }
