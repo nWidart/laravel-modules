3b8c5026fb905501601a26d54c8c67c811e5986a
Khoa Tran Dang <khoa.tran@pycogroup.com>
2020-01-20 22:04:12 +0700

    Update to support Lumen migration

---
+++ src/Commands/LumenMigrateCommand.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Nwidart\Modules\Commands;
+
+use Illuminate\Console\Command;
+use Nwidart\Modules\Migrations\LumenMigrator;
+use Nwidart\Modules\Module;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+class LumenMigrateCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'module:lumen-migrate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Migrate the migrations from the specified module or from all modules (For Lumen).';
+
+    /**
+     * @var \Nwidart\Modules\Contracts\RepositoryInterface
+     */
+    protected $module;
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->module = $this->laravel['modules'];
+
+        $name = $this->argument('module');
+
+        if ($name) {
+            $module = $this->module->findOrFail($name);
+
+            return $this->migrate($module);
+        }
+
+        foreach ($this->module->getOrdered($this->option('direction')) as $module) {
+            $this->line('Running for module: <info>' . $module->getName() . '</info>');
+
+            $this->migrate($module);
+        }
+    }
+
+    /**
+     * Run the migration from the specified module.
+     *
+     * @param Module $module
+     */
+    protected function migrate(Module $module)
+    {
+        $path = str_replace(base_path(), '', (new LumenMigrator($module, $this->getLaravel()))->getPath());
+
+        if ($this->option('subpath')) {
+            $path = $path . "/" . $this->option("subpath");
+        }
+
+        $this->call('migrate', [
+            '--path' => $path,
+            '--database' => $this->option('database'),
+            '--pretend' => $this->option('pretend'),
+            '--force' => $this->option('force'),
+        ]);
+
+        if ($this->option('seed')) {
+            $this->call('module:seed', ['module' => $module->getName()]);
+        }
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['module', InputArgument::OPTIONAL, 'The name of module will be used.'],
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['direction', 'd', InputOption::VALUE_OPTIONAL, 'The direction of ordering.', 'asc'],
+            ['database', null, InputOption::VALUE_OPTIONAL, 'The database connection to use.'],
+            ['pretend', null, InputOption::VALUE_NONE, 'Dump the SQL queries that would be run.'],
+            ['force', null, InputOption::VALUE_NONE, 'Force the operation to run when in production.'],
+            ['seed', null, InputOption::VALUE_NONE, 'Indicates if the seed task should be re-run.'],
+            ['subpath', null, InputOption::VALUE_OPTIONAL, 'Indicate a subpath to run your migrations from'],
+        ];
+    }
+}
