688bcf0487f8b391ced2ed3729508c7cf5a11c52
Dovydas Linkus <mcraft.mine412@gmail.com>
2020-10-07 20:29:34 +0100

    Ft: Appendable generated resource names

--- src/Generators/Command.php
+++ src/Generators/Command.php
@@ -2,9 +2,42 @@
 
 namespace Nwidart\Modules\Generators;
 
+use Illuminate\Support\Str;
 use Nwidart\Modules\Commands\GeneratorCommand;
 
 abstract class Command extends GeneratorCommand
 {
-    // To be extended ...
+
+    /**
+     * The name to be appended to the generated resources.
+     *
+     * @var null|string
+     */
+    protected $appendable;
+
+    /**
+     * Getter for appendable
+     *
+     * @return void
+     */
+    public function appendable()
+    {
+        return $this->appendable;
+    }
+
+    /**
+     * Get and resolve the filename.
+     *
+     * @return string
+     */
+    protected function getFileName(): string
+    {
+
+        $name = Str::studly($this->argument($this->argumentName));
+        if ($this->appendable() && !Str::contains(strtolower($name), strtolower($this->appendable()))) {
+            $name .= Str::studly($this->appendable());
+        }
+
+        return Str::singular(Str::studly($name));
+    }
 }
