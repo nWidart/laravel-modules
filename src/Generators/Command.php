ccb2831cc5133d89e33b9ac58b66ee1dac1b1c06
Dovydas Linkus <mcraft.mine412@gmail.com>
2020-10-07 21:00:47 +0100

    Update Command.php

--- src/Generators/Command.php
+++ src/Generators/Command.php
@@ -3,10 +3,14 @@
 namespace Nwidart\Modules\Generators;
 
 use Illuminate\Support\Str;
+use Nwidart\Modules\Support\Stub;
 use Nwidart\Modules\Commands\GeneratorCommand;
+use Nwidart\Modules\Console\Traits\Definitions;
+use Nwidart\Modules\Support\Config\GenerateConfigReader;
 
 abstract class Command extends GeneratorCommand
 {
+    use Definitions;
 
     /**
      * The name to be appended to the generated resources.
@@ -16,6 +20,13 @@ abstract class Command extends GeneratorCommand
     protected $appendable;
 
     /**
+     * Stub file of the resource
+     *
+     * @var null|string
+     */
+    protected $stubFile;
+
+    /**
      * Getter for appendable
      *
      * @return void
@@ -23,6 +34,87 @@ abstract class Command extends GeneratorCommand
     public function appendable()
     {
         return $this->appendable;
+    }
+
+    /**
+     * Getter for the stub file
+     *
+     * @return void
+     */
+    public function stubFile()
+    {
+        return $this->stubFile;
+    }
+
+    /**
+     * Replacements for the stub file
+     *
+     * @return array
+     */
+    public function replaces()
+    {
+        return [
+            //...
+        ];
+    }
+
+    /**
+     * Get stub file contents
+     *
+     * @return mixed
+     */
+    protected function getTemplateContents()
+    {
+        return (new Stub($this->stubFile(), $this->replaces()))->render();
+    }
+
+    /**
+     * Get destination file path.
+     *
+     * @return mixed
+     */
+    public function getDestinationFilePath()
+    {
+        return  $this->getModulePath() . "/" .
+            GenerateConfigReader::read($this->getGeneratorConfigKey())->getPath() . "/" .
+            $this->resolveFilename() . '.' . $this->outputExtension;
+    }
+
+    /**
+     * Method to apply necessary functionality
+     * before console command gets executed
+     *
+     * @return void
+     */
+    public function before()
+    {
+        // ...
+    }
+
+    /**
+     * Method to apply necessary functionality
+     * after console command has executed.
+     *
+     * @return void
+     */
+    public function after()
+    {
+        // ...
+    }
+
+    /**
+     * Execute the console command
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $this->before();
+
+        if (parent::handle() != E_ERROR) {
+            $this->after();
+        }
+        return 0;
     }
 
     /**
