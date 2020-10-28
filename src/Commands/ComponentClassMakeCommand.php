7dab28b177b7b6349e49a498707e33abe76dd0e2
ahmedggalal <ahmed_ggalal@yahoo.com>
2020-10-28 02:55:59 +0200

    Create Component Class

---
+++ src/Commands/ComponentClassMakeCommand.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Nwidart\Modules\Commands;
+
+use Illuminate\Support\Str;
+use Nwidart\Modules\Support\Config\GenerateConfigReader;
+use Nwidart\Modules\Support\Stub;
+use Nwidart\Modules\Traits\ModuleCommandTrait;
+use Symfony\Component\Console\Input\InputArgument;
+
+class ComponentClassMakeCommand extends GeneratorCommand
+{
+    use ModuleCommandTrait;
+
+    /**
+     * The name of argument name.
+     *
+     * @var string
+     */
+    protected $argumentName = 'name';
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'module:make-component';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new component-class for the specified module.';
+
+
+
+    public function getDefaultNamespace() : string
+    {
+        $module = $this->laravel['modules'];
+        return $module->config('paths.generator.component-class.namespace') ?: $module->config('paths.generator.component-class.path', 'View/Component');
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the component.'],
+            ['module', InputArgument::OPTIONAL, 'The name of module will be used.'],
+        ];
+    }
+    /**
+     * @return mixed
+     */
+    protected function getTemplateContents()
+    {
+        $module = $this->laravel['modules']->findOrFail($this->getModuleName());
+
+        return (new Stub('/component-class.stub', [
+            'NAMESPACE'         => $this->getClassNamespace($module),
+            'CLASS'             => $this->getClass()
+        ]))->render();
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getDestinationFilePath()
+    {
+        $path = $this->laravel['modules']->getModulePath($this->getModuleName());
+        $factoryPath = GenerateConfigReader::read('component-class');
+        return $path . $factoryPath->getPath() . '/' . $this->getFileName();
+    }
+
+    /**
+     * @return string
+     */
+    private function getFileName()
+    {
+        return Str::studly($this->argument('name')) . '.php';
+    }
+
+}
