e23300b0deb453f47cc00366bd3338510d8548e4
Jimmi Hansen <jimmi@xevotech.com>
2020-12-20 17:16:23 +0100

    Fix failing test
    Co-Authored-By: Nicolas Widart <n.widart@gmail.com>

--- tests/Database/LaravelEloquentRepositoryTest.php
+++ tests/Database/LaravelEloquentRepositoryTest.php
@@ -16,7 +16,7 @@ class LaravelEloquentRepositoryTest extends BaseTestCase
      */
     private $repository;
 
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
         $this->repository = app(LaravelEloquentRepository::class);
