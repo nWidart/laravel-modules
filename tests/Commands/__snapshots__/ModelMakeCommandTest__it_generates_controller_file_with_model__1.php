da4a79a0b7cf0ce967103a3133008fef883c0a01
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2021-08-12 11:50:41 +0430

    rename snapshot controller name

--- tests/Commands/__snapshots__/ModelMakeCommandTest__it_generates_controller_file_with_model__1.php
+++ tests/Commands/__snapshots__/ModelMakeCommandTest__it_generates_controller_file_with_model__1.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\\Http\\Request;
 use Illuminate\\Routing\\Controller;
 
-class MyController extends Controller
+class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
