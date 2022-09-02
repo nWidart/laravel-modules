d2d3a11742d4fa13b7e89d7e266d1ff1fda080ba
wikigods <wikigods@gmail.com>
2022-09-02 05:40:57 -0500

    Add test vite

---
+++ tests/Commands/__snapshots__/ModuleMakeCommandTest__it_generates_vite_file__1.php
@@ -0,0 +1,23 @@
+<?php
+return 'const dotenvExpand = require(\'dotenv-expand\');
+dotenvExpand(require(\'dotenv\').config({ path: \'../../.env\'/*, debug: true*/}));
+
+import { defineConfig } from \'vite\';
+//import laravel from \'laravel-vite-plugin\';
+
+export default defineConfig({
+    build: {
+    outDir: \'../../public/build-blog\',
+        emptyOutDir: true,
+        manifest: true,
+        target: \'es2018\',
+        rollupOptions: {
+        input: [
+            __dirname + \'/Resources/assets/sass/app.scss\',
+            __dirname + \'/Resources/assets/js/app.js\'
+        ]
+        }
+    },
+});
+';
+
