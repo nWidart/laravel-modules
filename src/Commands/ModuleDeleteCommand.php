4aa30561de7c775465240a07246e83257cc177a8
Solomon Ochepa <solomonochepa@gmail.com>
2024-02-03 20:13:54 -0800

    Ability to delete multiple modules
    Added the ability to delete multiple modules at once.
    
    ## Usage:
    ### Delete a Module
    `php artisan module:delete  Module1
    
    ### Delete Multiple Modules
    `php artisan module:delete  Module1 Module2 Module3

--- src/Commands/ModuleDeleteCommand.php
+++ src/Commands/ModuleDeleteCommand.php
@@ -8,13 +8,16 @@ use Symfony\Component\Console\Input\InputArgument;
 class ModuleDeleteCommand extends Command
 {
     protected $name = 'module:delete';
-    protected $description = 'Delete a module from the application';
+    protected $description = 'Delete module(s) from the application.';
 
     public function handle(): int
     {
-        $this->laravel['modules']->delete($this->argument('module'));
+        $modules = $this->argument('module');
 
-        $this->components->info("Module {$this->argument('module')} has been deleted.");
+        foreach ($modules as $module) {
+            $this->laravel['modules']->delete($module);
+            $this->components->info("Module {$module} has been deleted.");
+        }
 
         return 0;
     }
@@ -22,7 +25,7 @@ class ModuleDeleteCommand extends Command
     protected function getArguments()
     {
         return [
-            ['module', InputArgument::REQUIRED, 'The name of module to delete.'],
+            ['module', InputArgument::IS_ARRAY, 'The name of module to delete.'],
         ];
     }
 }
