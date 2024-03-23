75fcd0f4771bafc352fd8f1e2d75f653b787b6f6
Solomon Ochepa <solomonochepa@gmail.com>
2024-03-23 04:31:13 -0700

    Update config.php
    * Remove repeated replacement key for `routes/api`
    * Enable `app/Models/` path generator (`database/*` paths are enabled by default, therefore models/ will be needed)
    * Format comments to collapsible and remove space between the comment and its related property.

--- config/config.php
+++ config/config.php
@@ -4,27 +4,24 @@ use Nwidart\Modules\Activators\FileActivator;
 use Nwidart\Modules\Providers\ConsoleServiceProvider;
 
 return [
-
     /*
-    |--------------------------------------------------------------------------
-    | Module Namespace
-    |--------------------------------------------------------------------------
-    |
-    | Default module namespace.
-    |
+     *--------------------------------------------------------------------------
+     * Module Namespace
+     *--------------------------------------------------------------------------
+     *
+     * Default module namespace.
+     *
     */
-
     'namespace' => 'Modules',
 
     /*
-    |--------------------------------------------------------------------------
-    | Module Stubs
-    |--------------------------------------------------------------------------
-    |
-    | Default module stubs.
-    |
+     *--------------------------------------------------------------------------
+     * Module Stubs
+     *--------------------------------------------------------------------------
+     *
+     * Default module stubs.
+     *
     */
-
     'stubs' => [
         'enabled' => false,
         'path' => base_path('vendor/nwidart/laravel-modules/src/Commands/stubs'),
@@ -42,7 +39,7 @@ return [
         ],
         'replacements' => [
             'routes/web' => ['LOWER_NAME', 'STUDLY_NAME', 'MODULE_NAMESPACE', 'CONTROLLER_NAMESPACE'],
-            'routes/api' => ['LOWER_NAME', 'STUDLY_NAME', 'MODULE_NAMESPACE', 'STUDLY_NAME', 'CONTROLLER_NAMESPACE'],
+            'routes/api' => ['LOWER_NAME', 'STUDLY_NAME', 'MODULE_NAMESPACE', 'CONTROLLER_NAMESPACE'],
             'vite' => ['LOWER_NAME', 'STUDLY_NAME'],
             'json' => ['LOWER_NAME', 'STUDLY_NAME', 'MODULE_NAMESPACE', 'PROVIDER_NAMESPACE'],
             'views/index' => ['LOWER_NAME'],
@@ -62,54 +59,53 @@ return [
     ],
     'paths' => [
         /*
-        |--------------------------------------------------------------------------
-        | Modules path
-        |--------------------------------------------------------------------------
-        |
-        | This path is used to save the generated module.
-        | This path will also be added automatically to the list of scanned folders.
-        |
+         *--------------------------------------------------------------------------
+         * Modules path
+         *--------------------------------------------------------------------------
+         *
+         * This path is used to save the generated module.
+         * This path will also be added automatically to the list of scanned folders.
+         *
         */
-
         'modules' => base_path('Modules'),
-        /*
-        |--------------------------------------------------------------------------
-        | Modules assets path
-        |--------------------------------------------------------------------------
-        |
-        | Here you may update the modules' assets path.
-        |
-        */
 
+        /*
+         *--------------------------------------------------------------------------
+         * Modules assets path
+         *--------------------------------------------------------------------------
+         *
+         * Here you may update the modules' assets path.
+         *
+        */
         'assets' => public_path('modules'),
-        /*
-        |--------------------------------------------------------------------------
-        | The migrations' path
-        |--------------------------------------------------------------------------
-        |
-        | Where you run the 'module:publish-migration' command, where do you publish the
-        | the migration files?
-        |
-        */
 
+        /*
+         *--------------------------------------------------------------------------
+         * The migrations' path
+         *--------------------------------------------------------------------------
+         *
+         * Where you run the 'module:publish-migration' command, where do you publish the
+         * the migration files?
+         *
+        */
         'migration' => base_path('database/migrations'),
 
         /*
-        |--------------------------------------------------------------------------
-        | The app path
-        |--------------------------------------------------------------------------
-        |
-        | app folder name
-        | for example can change it to 'src' or 'App'
+         *--------------------------------------------------------------------------
+         * The app path
+         *--------------------------------------------------------------------------
+         *
+         * app folder name
+         * for example can change it to 'src' or 'App'
         */
         'app_folder' => 'app/',
 
         /*
-        |--------------------------------------------------------------------------
-        | Generator path
-        |--------------------------------------------------------------------------
-        | Customise the paths where the folders will be generated.
-        | Setting the generate key to false will not generate that folder
+         *--------------------------------------------------------------------------
+         * Generator path
+         *--------------------------------------------------------------------------
+         * Customise the paths where the folders will be generated.
+         * Setting the generate key to false will not generate that folder
         */
         'generator' => [
             // app/
@@ -119,7 +115,7 @@ return [
             'event' => ['path' => 'app/Events', 'generate' => false],
             'jobs' => ['path' => 'app/Jobs', 'generate' => false],
             'listener' => ['path' => 'app/Listeners', 'generate' => false],
-            'model' => ['path' => 'app/Models', 'generate' => false],
+            'model' => ['path' => 'app/Models', 'generate' => true],
             'notifications' => ['path' => 'app/Notifications', 'generate' => false],
             'observer' => ['path' => 'app/Observers', 'generate' => false],
             'policies' => ['path' => 'app/Policies', 'generate' => false],
@@ -160,13 +156,13 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Package commands
-    |--------------------------------------------------------------------------
-    |
-    | Here you can define which commands will be visible and used in your
-    | application. You can add your own commands to merge section.
-    |
+     *--------------------------------------------------------------------------
+     * Package commands
+     *--------------------------------------------------------------------------
+     *
+     * Here you can define which commands will be visible and used in your
+     * application. You can add your own commands to merge section.
+     *
     */
     'commands' => ConsoleServiceProvider::defaultCommands()
         ->merge([
@@ -174,30 +170,29 @@ return [
         ])->toArray(),
 
     /*
-    |--------------------------------------------------------------------------
-    | Scan Path
-    |--------------------------------------------------------------------------
-    |
-    | Here you define which folder will be scanned. By default will scan vendor
-    | directory. This is useful if you host the package in packagist website.
-    |
+     *--------------------------------------------------------------------------
+     * Scan Path
+     *--------------------------------------------------------------------------
+     *
+     * Here you define which folder will be scanned. By default will scan vendor
+     * directory. This is useful if you host the package in packagist website.
+     *
     */
-
     'scan' => [
         'enabled' => false,
         'paths' => [
             base_path('vendor/*/*'),
         ],
     ],
-    /*
-    |--------------------------------------------------------------------------
-    | Composer File Template
-    |--------------------------------------------------------------------------
-    |
-    | Here is the config for the composer.json file, generated by this package
-    |
-    */
 
+    /*
+     *--------------------------------------------------------------------------
+     * Composer File Template
+     *--------------------------------------------------------------------------
+     *
+     * Here is the config for the composer.json file, generated by this package
+     *
+    */
     'composer' => [
         'vendor' => 'nwidart',
         'author' => [
@@ -208,12 +203,12 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Caching
-    |--------------------------------------------------------------------------
-    |
-    | Here is the config for setting up the caching feature.
-    |
+     *--------------------------------------------------------------------------
+     * Caching
+     *--------------------------------------------------------------------------
+     *
+     * Here is the config for setting up the caching feature.
+     *
     */
     'cache' => [
         'enabled' => false,
@@ -221,12 +216,13 @@ return [
         'key' => 'laravel-modules',
         'lifetime' => 60,
     ],
+
     /*
-    |--------------------------------------------------------------------------
-    | Choose what laravel-modules will register as custom namespaces.
-    | Setting one to false will require you to register that part
-    | in your own Service Provider class.
-    |--------------------------------------------------------------------------
+     *--------------------------------------------------------------------------
+     * Choose what laravel-modules will register as custom namespaces.
+     * Setting one to false will require you to register that part
+     * in your own Service Provider class.
+     *--------------------------------------------------------------------------
     */
     'register' => [
         'translations' => true,
@@ -237,13 +233,13 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Activators
-    |--------------------------------------------------------------------------
-    |
-    | You can define new types of activators here, file, database, etc. The only
-    | required parameter is 'class'.
-    | The file activator will store the activation status in storage/installed_modules
+     *--------------------------------------------------------------------------
+     * Activators
+     *--------------------------------------------------------------------------
+     *
+     * You can define new types of activators here, file, database, etc. The only
+     * required parameter is 'class'.
+     * The file activator will store the activation status in storage/installed_modules
     */
     'activators' => [
         'file' => [
