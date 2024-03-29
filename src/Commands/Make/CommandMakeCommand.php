2642a5d92643c7c984f7d76fc4d9653a602b7cf8
Solomon O. Ochepa <solomonochepa@gmail.com>
2024-03-29 23:18:00 +0100

    [fix] Update commands namespace

--- src/Commands/Make/CommandMakeCommand.php
+++ src/Commands/Make/CommandMakeCommand.php
@@ -36,8 +36,7 @@ class CommandMakeCommand extends GeneratorCommand
 
     public function getDefaultNamespace(): string
     {
-        return config('modules.paths.generator.command.namespace')
-            ?? ltrim(config('modules.paths.generator.command.path', 'Console'), config('modules.paths.app_folder', ''));
+        return config('modules.paths.generator.command.namespace') ?? $this->getPathNamespace(config('modules.paths.generator.command.path', 'app/Console'));
     }
 
     /**
