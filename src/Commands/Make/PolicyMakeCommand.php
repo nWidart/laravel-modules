4f29baa9b05d1f9cc776f61f86e7f27ea2a2334e
Solomon O. Ochepa <solomonochepa@gmail.com>
2024-03-29 23:29:13 +0100

    [fix] Update policies namespace

--- src/Commands/Make/PolicyMakeCommand.php
+++ src/Commands/Make/PolicyMakeCommand.php
@@ -35,8 +35,7 @@ class PolicyMakeCommand extends GeneratorCommand
 
     public function getDefaultNamespace(): string
     {
-        return config('modules.paths.generator.policies.namespace')
-            ?? ltrim(config('modules.paths.generator.policies.path', 'Policies'), config('modules.paths.app_folder', ''));
+        return config('modules.paths.generator.policies.namespace') ?? $this->getPathNamespace(config('modules.paths.generator.policies.path', 'app/Policies'));
     }
 
     /**
