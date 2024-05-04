2ce3880a04b10512ea7890f18c5e81b7df54755f
Solomon Ochepa <solomonochepa@gmail.com>
2024-05-04 06:28:55 +0100

    Create clean_path() method - PathNamespace

--- src/Traits/PathNamespace.php
+++ src/Traits/PathNamespace.php
@@ -9,20 +9,17 @@ trait PathNamespace
     /**
      * Get a well-formatted StudlyCase representation of path components.
      */
-    public function studly_path(string $path, $directory_separator = '/'): string
+    public function studly_path(string $path, $ds = '/'): string
     {
-        return collect(explode($directory_separator, Str::of($path)
-            ->replace("{$directory_separator}{$directory_separator}", $directory_separator)->trim($directory_separator)))
-            ->map(fn ($path) => Str::studly($path))
-            ->implode($directory_separator);
+        return collect(explode($ds, $this->clean_path($path, $ds)))->map(fn ($path) => Str::studly($path))->implode($ds);
     }
 
     /**
      * Get a well-formatted StudlyCase namespace.
      */
-    public function studly_namespace(string $namespace, $directory_separator = '\\'): string
+    public function studly_namespace(string $namespace, $ds = '\\'): string
     {
-        return $this->studly_path($namespace, $directory_separator);
+        return $this->studly_path($namespace, $ds);
     }
 
     /**
@@ -36,11 +33,19 @@ trait PathNamespace
     /**
      * Get a well-formatted StudlyCase namespace for a module, with an optional additional path.
      */
-    public function module_namespace(string $module, string $path = null): string
+    public function module_namespace(string $module, ?string $path = null): string
     {
         $module_namespace = config('modules.namespace', $this->path_namespace(config('modules.paths.modules'))) . '\\' . ($module);
         $module_namespace .= strlen($path) ? '\\' . $this->path_namespace($path) : '';
 
         return $this->studly_namespace($module_namespace);
+    }
+
+    /**
+     * Clean path
+     */
+    public function clean_path(string $path, $ds = '/'): string
+    {
+        return Str::of($path)->explode($ds)->reject(empty($path))->implode($ds);
     }
 }
