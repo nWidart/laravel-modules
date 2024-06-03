71f47715579f9210419d45eccb2fc0e6f7664cf0
Ali Sasani <a_S47_1@yahoo.com>
2024-06-04 01:06:31 +0330

    [feat] change comamnd module:delete to ConfirmableCommand

--- src/Commands/Actions/ModuleDeleteCommand.php
+++ src/Commands/Actions/ModuleDeleteCommand.php
@@ -2,12 +2,27 @@
 
 namespace Nwidart\Modules\Commands\Actions;
 
+use Illuminate\Console\ConfirmableTrait;
+use Illuminate\Console\Prohibitable;
 use Nwidart\Modules\Commands\BaseCommand;
+use Nwidart\Modules\Contracts\ConfirmableCommand;
 
-class ModuleDeleteCommand extends BaseCommand
+class ModuleDeleteCommand extends BaseCommand implements ConfirmableCommand
 {
+    use ConfirmableTrait, Prohibitable;
+
     protected $name        = 'module:delete';
     protected $description = 'Delete a module from the application';
+
+    public function handle()
+    {
+        if ($this->isProhibited() ||
+            ! $this->confirmToProceed('Warning: Do you want to remove the module?', fn () => TRUE)) {
+            return 1;
+        }
+
+        parent::handle();
+    }
 
     public function executeAction($name): void
     {
