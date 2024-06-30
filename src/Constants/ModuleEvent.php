98df97e2f2e87ddba4044fad5e50b9e161fd4511
Ali Sasani <a_S47_1@yahoo.com>
2024-07-01 00:45:17 +0330

    [feat] add ModuleEvent constants

---
+++ src/Constants/ModuleEvent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Nwidart\Modules\Constants;
+
+class ModuleEvent
+{
+    const BOOT = 'boot';
+
+    const REGISTER = 'register';
+
+    const DISABLING = 'disabling';
+
+    const DISABLED = 'disabled';
+
+    const ENABLING = 'enabling';
+
+    const ENABLED = 'enabled';
+
+    const CREATE = 'create';
+
+    const DELETE = 'delete';
+
+    const USED = 'used';
+
+    const UNUSED = 'unused';
+}
