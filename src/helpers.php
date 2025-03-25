4feac6466d5c35091fbed13ad26ff8308bae4129
Solomon Ochepa <solomonochepa@gmail.com>
2025-03-25 20:15:38 +0100

    Create `module()` helper function

--- src/helpers.php
+++ src/helpers.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Foundation\Vite;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Vite as ViteFacade;
+use Nwidart\Modules\Laravel\Module;
+
+if (! function_exists('module')) {
+    /**
+     * Retrieves a module instance or its status.
+     *
+     * @param  string  $name  The name of the module.
+     * @param  bool  $status  Whether to return the module's status instead of the instance. Defaults to false.
+     * @return Module|bool The module instance or its status.
+     */
+    function module(string $name, bool $status = false): Module|bool
+    {
+        $modules = app('modules');
+        if (! $modules->has($name)) {
+            Log::error("Module '$name' not found.");
+
+            return false;
+        }
+
+        return $status ? $modules->isEnabled($name) : $modules->find($name);
+    }
+}
 
 if (! function_exists('module_path')) {
     function module_path(string $name, string $path = ''): string
