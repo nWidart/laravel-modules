dc6306155b22e51933dc210bd6c6985317a247f0
Solomon Ochepa <solomonochepa@gmail.com>
2025-05-05 15:20:14 +0100

    feat: add language files for Recipe module with initial translations

---
+++ tests/stubs/valid/Recipe/lang/en/recipes.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'title' => [
+        'recipes' => 'Recipe',
+        'create recipe' => 'Create a recipe',
+        'edit recipe' => 'Edit a recipe',
+    ],
+    'button' => [
+        'create recipe' => 'Create a recipe',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+];
